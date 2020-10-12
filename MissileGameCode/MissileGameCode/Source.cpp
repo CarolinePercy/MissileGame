ff635c00232d3676d854483a05ebdbef519c00e7
Caroline Percy <lineypercy@me.com>
2020-10-12 09:34:14 +0100

    Basic Code Setup

---
+++ MissileGameCode/MissileGameCode/Source.cpp
@@ -0,0 +1,13 @@
+/// <summary>
+/// Author: Caroline Percy
+/// Date: 12 Oct 2020 - 
+/// Objective: Create a console missile game, where the player fires a missile which explodes when it hits the target, or deactivates if it misses
+/// Bugs: None
+/// </summary>
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
